1bec9f9f596881072c4036e625af5eb2f33c0fda
Sanjana Khajuria <sanjana.newgit@gmail.com>
2026-08-24 03:14:16 +0530

    longest substring palindrome

---
+++ pal_substr.py
@@ -0,0 +1,17 @@
+x = input("Enter: ")
+def expand(x,left,right):
+    while left >= 0 and right < len(x) and x[left] == x[right]:
+        left -= 1
+        right += 1
+    return x[left+1:right]
+def long(x):
+    longest = ""
+    for i in range(len(x)):
+        odd = expand(x,i,i)
+        even = expand(x,i,i+1)
+        if len(odd) > len(longest):
+            longest = odd
+        if len(even) > len(longest):
+            longest = even
+    return longest
+print(long(x))
