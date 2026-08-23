688997e49051731bb8805c7ae4f6b4303f87ca45
Sanjana Khajuria <sanjana.newgit@gmail.com>
2026-08-23 20:14:08 +0530

    merging intervals

--- merge_intervals.py
+++ merge_intervals.py
@@ -4,14 +4,10 @@ def merge(arr):
     result = [arr[0]]
     for i in range(1,len(arr)):
         last = result[-1]
-        print("last = ",last)
         curr = arr[i]
-        print("Curr= ",curr)
         if curr[0] <= last[1]:
             last[1] = max(last[1],curr[1])
-            print("new last: ",last)
         else:
             result.append(curr)
-            print("result: ",result)
     return result
 print(merge(x))
