895c0325aadee5ec2b7f969ea1b4f4b7f1604915
Sanjana Khajuria <sanjana.newgit@gmail.com>
2026-08-23 20:49:57 +0530

    rotated array

---
+++ rotated.py
@@ -0,0 +1,21 @@
+x = list(map(int, input("Enter: ").split()))
+t = int(input("Enter target: "))
+def rotate(arr,k):
+    left = 0
+    right = len(arr) - 1
+    while left <= right:
+        mid = (left+right)//2
+        if arr[mid] == k:
+            return mid
+        if arr[left] <= arr[mid]:
+            if arr[left] <= k <arr[mid]:
+                right = mid-1
+            else:
+                left = mid+1
+        else:
+            if arr[mid] < k <= arr[right]:
+                left = mid+1
+            else:
+                right = mid-1
+    return -1
+print(rotate(x,t))
