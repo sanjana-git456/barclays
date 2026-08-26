67679a17a26c7cb3b63dd5f3a73c85ee3172cff4
Sanjana Khajuria <sanjana.newgit@gmail.com>
2026-08-26 23:31:03 +0530

    reversing words manual method

--- reverse_words.py
+++ reverse_words.py
@@ -3,3 +3,20 @@ def rev(x):
     x = x.split()
     return x[::-1]
 print(' '.join(rev(x)))
+
+def manual(x):
+    words = []
+    word = ""
+    i = 0
+    while i < len(x):
+        if x[i] != " ":
+            word += x[i]
+        else:
+            if word:
+                words.append(word)
+                word = ""
+        i += 1
+    if word:
+        words.append(word)
+    return words
+print(manual(x))
